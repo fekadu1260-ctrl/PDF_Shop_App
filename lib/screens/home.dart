1782fe4cbe55a073a5bc6f60d22b29f2124599c3
fekadu1260-ctrl <fekadu1260@gmail.com>
2026-08-02 12:39:29 +0300

    Add home screen

---
+++ lib/screens/home.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("PDF Shop"),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+          const Text(
+            "Welcome!",
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
+          const SizedBox(height: 20),
+
+          Card(
+            child: ListTile(
+              leading: const Icon(Icons.menu_book),
+              title: const Text("Engineering PDFs"),
+              subtitle: const Text("Civil, Electrical, Mechanical"),
+            ),
+          ),
+
+          Card(
+            child: ListTile(
+              leading: const Icon(Icons.school),
+              title: const Text("University Notes"),
+              subtitle: const Text("Lecture notes and exams"),
+            ),
+          ),
+
+          Card(
+            child: ListTile(
+              leading: const Icon(Icons.download),
+              title: const Text("My Downloads"),
+            ),
+          ),
+
+          Card(
+            child: ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text("My Profile"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
