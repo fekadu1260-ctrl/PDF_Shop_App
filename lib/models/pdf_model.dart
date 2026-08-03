33e991398b858cae12bf7d067a75b1dd623097a2
fekadu1260-ctrl <fekadu1260@gmail.com>
2026-08-03 22:29:32 +0300

    Add PDF model

---
+++ lib/models/pdf_model.dart
@@ -0,0 +1,25 @@
+class PdfModel {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String fileUrl;
+
+  PdfModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.fileUrl,
+  });
+
+  factory PdfModel.fromJson(Map<String, dynamic> json) {
+    return PdfModel(
+      id: json['_id'] ?? '',
+      title: json['title'] ?? '',
+      description: json['description'] ?? '',
+      price: (json['price'] ?? 0).toDouble(),
+      fileUrl: json['fileUrl'] ?? '',
+    );
+  }
+}
