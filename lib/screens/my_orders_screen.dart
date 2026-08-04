cb0208b7b7609f65143b6a23b8bd068a9c946cd5
fekadu1260-ctrl <fekadu1260@gmail.com>
2026-08-04 11:08:37 +0300

    Create my orders screen

---
+++ lib/screens/my_orders_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MyOrdersScreen extends StatelessWidget {
+  const MyOrdersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        title: const Text("My Purchases"),
+      ),
+
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+
+        children: [
+
+          Card(
+            child: ListTile(
+              leading: const Icon(Icons.picture_as_pdf),
+              title: const Text("Sample PDF"),
+              subtitle: const Text("Status: Pending Payment"),
+              trailing: const Icon(Icons.arrow_forward),
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
