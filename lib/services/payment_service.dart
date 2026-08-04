ab3a0eeaafae1c35f598b5ee577388b051a34823
fekadu1260-ctrl <fekadu1260@gmail.com>
2026-08-04 13:02:02 +0300

    Create payment service

---
+++ lib/services/payment_service.dart
@@ -0,0 +1,83 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+
+class PaymentService {
+
+
+  Future<bool> createPayment({
+
+    required String userId,
+    required String pdfId,
+    required double amount,
+
+  }) async {
+
+
+    final response = await http.post(
+
+      Uri.parse(
+        "http://localhost:3000/payments",
+      ),
+
+
+      headers: {
+
+        "Content-Type": "application/json",
+
+      },
+
+
+      body: jsonEncode({
+
+        "userId": userId,
+
+        "pdfId": pdfId,
+
+        "amount": amount,
+
+        "status": "pending",
+
+      }),
+
+    );
+
+
+    return response.statusCode == 200 ||
+        response.statusCode == 201;
+
+  }
+
+
+
+  Future<String> checkPaymentStatus(
+      String paymentId) async {
+
+
+    final response = await http.get(
+
+      Uri.parse(
+        "http://localhost:3000/payments/$paymentId",
+      ),
+
+    );
+
+
+    if(response.statusCode == 200){
+
+      final data = jsonDecode(
+        response.body,
+      );
+
+
+      return data['status'];
+
+    }
+
+
+    return "pending";
+
+  }
+
+
+}
