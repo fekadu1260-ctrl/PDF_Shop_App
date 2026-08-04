83544996e42f3ac50f4f51ecfc0fcd458658381e
fekadu1260-ctrl <fekadu1260@gmail.com>
2026-08-04 12:30:48 +0300

    Create admin PDF upload service

---
+++ lib/services/admin_pdf_service.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class AdminPdfService {
+
+  Future<bool> uploadPdf({
+    required String title,
+    required double price,
+    required String fileUrl,
+  }) async {
+
+    final response = await http.post(
+
+      Uri.parse("http://localhost:3000/pdfs"),
+
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: jsonEncode({
+
+        "title": title,
+        "price": price,
+        "fileUrl": fileUrl,
+
+      }),
+
+    );
+
+
+    return response.statusCode == 200 ||
+        response.statusCode == 201;
+
+  }
+
+}
