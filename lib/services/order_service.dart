ea1b700a0b38b7266c7ac49f9ee5d174d7e613f7
fekadu1260-ctrl <fekadu1260@gmail.com>
2026-08-04 10:37:04 +0300

    Create order service

---
+++ lib/services/order_service.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/order_model.dart';
+
+class OrderService {
+
+  Future<OrderModel> createOrder({
+    required String userId,
+    required String pdfId,
+    required double amount,
+  }) async {
+
+    final response = await http.post(
+
+      Uri.parse("http://localhost:3000/orders"),
+
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: jsonEncode({
+
+        "userId": userId,
+        "pdfId": pdfId,
+        "amount": amount,
+        "status": "pending",
+
+      }),
+    );
+
+
+    if (response.statusCode == 200 ||
+        response.statusCode == 201) {
+
+      return OrderModel.fromJson(
+        jsonDecode(response.body),
+      );
+
+    } else {
+
+      throw Exception(
+        "Order creation failed",
+      );
+
+    }
+  }
+}
