6d21429befe74badf97da9528e8b925f6a71ca14
fekadu1260-ctrl <fekadu1260@gmail.com>
2026-08-04 12:44:40 +0300

    Add user role service

---
+++ lib/services/user_service.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/user_model.dart';
+
+
+class UserService {
+
+
+  Future<UserModel?> getUser(
+      String email) async {
+
+
+    final response = await http.get(
+
+      Uri.parse(
+        "http://localhost:3000/users/$email",
+      ),
+
+    );
+
+
+    if(response.statusCode == 200){
+
+      final data = jsonDecode(
+        response.body,
+      );
+
+
+      return UserModel.fromJson(data);
+
+    }
+
+
+    return null;
+
+  }
+
+
+
+  Future<bool> isAdmin(
+      String email) async {
+
+
+    final user = await getUser(email);
+
+
+    if(user == null){
+
+      return false;
+
+    }
+
+
+    return user.role == "admin";
+
+  }
+
+
+}
