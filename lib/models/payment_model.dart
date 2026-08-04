03f56f3e7f8d828a83f42101f6e51eda533dc180
fekadu1260-ctrl <fekadu1260@gmail.com>
2026-08-04 13:00:36 +0300

    Create payment model

---
+++ lib/models/payment_model.dart
@@ -0,0 +1,41 @@
+class PaymentModel {
+
+  final String id;
+  final String userId;
+  final String pdfId;
+  final double amount;
+  final String status;
+
+
+  PaymentModel({
+
+    required this.id,
+    required this.userId,
+    required this.pdfId,
+    required this.amount,
+    required this.status,
+
+  });
+
+
+
+  factory PaymentModel.fromJson(
+      Map<String, dynamic> json) {
+
+    return PaymentModel(
+
+      id: json['_id'] ?? '',
+
+      userId: json['userId'] ?? '',
+
+      pdfId: json['pdfId'] ?? '',
+
+      amount: (json['amount'] ?? 0).toDouble(),
+
+      status: json['status'] ?? 'pending',
+
+    );
+
+  }
+
+}
