59c446204af007d5d193972e0eabf54d940f333e
fekadu1260-ctrl <fekadu1260@gmail.com>
2026-08-04 12:52:10 +0300

    Add PDF file picker service

---
+++ lib/services/file_picker_service.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
+
+
+class FilePickerService {
+
+
+  Future<File?> pickPdf() async {
+
+
+    final result =
+        await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: [
+        'pdf'
+      ],
+    );
+
+
+    if(result != null){
+
+      return File(
+        result.files.single.path!,
+      );
+
+    }
+
+
+    return null;
+
+  }
+
+}
