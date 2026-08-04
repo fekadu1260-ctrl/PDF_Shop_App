881f093941d23cd4e5ed153ff41295f5d915d28f
fekadu1260-ctrl <fekadu1260@gmail.com>
2026-08-04 12:21:36 +0300

    Create category service

---
+++ lib/services/category_service.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/category_model.dart';
+
+class CategoryService {
+
+  Future<List<CategoryModel>> fetchCategories() async {
+
+    final response = await http.get(
+      Uri.parse("http://localhost:3000/categories"),
+    );
+
+    if(response.statusCode == 200){
+
+      final data = jsonDecode(response.body);
+
+      return data.map<CategoryModel>((json){
+
+        return CategoryModel.fromJson(json);
+
+      }).toList();
+
+    }else{
+
+      throw Exception("Failed to load categories");
+
+    }
+
+  }
+
+}
