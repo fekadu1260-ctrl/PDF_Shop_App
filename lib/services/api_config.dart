2c3f9357200a2563a05e32dbf481da5cfda0136f
Fekadu Kefale <yourname@gmail.com>
2026-08-21 04:37:55 +0300

    Fix API server address for local network

--- lib/services/api_config.dart
+++ lib/services/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   // Development server running through Termux.
-  static const String localBaseUrl = "http://10.89.172.176:3000";
+  static const String localBaseUrl = "http://192.168.8.8:3000";
 
   // Production API will go here when deployed.
   static const String productionBaseUrl = "https://YOUR-PRODUCTION-API";
