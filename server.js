5cde35f6dbde4d0f54e4a33d2cb1dfb80b268c39
root <root@localhost.localdomain>
2026-08-22 07:46:46 +0300

    Use Firebase service account from environment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 const { getAuth } = require("firebase-admin/auth");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
 
 initializeApp({
   credential: cert(serviceAccount)
