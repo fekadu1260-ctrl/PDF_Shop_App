082cbcfeac8247973828dbbdba2950746f19a802
Fekadu Kefale <yourname@gmail.com>
2026-08-24 10:04:10 +0300

    Fix production API hostname

--- lib/services/api_config.dart
+++ lib/services/api_config.dart
@@ -3,7 +3,7 @@ class ApiConfig {
   static const String localBaseUrl = "http://192.168.8.8:3000";
 
   // Production API will go here when deployed.
-  static const String productionBaseUrl = "https://pdf-shop-app-api.onrender.com";
+  static const String productionBaseUrl = "https://pdf-shop-api.onrender.com";
 
   // Current development mode.
   static const bool production = true;
